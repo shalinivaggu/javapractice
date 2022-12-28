f8cfc02c2b1c89cef4199c60872a4adb7edd4651
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2022-12-28 15:23:25 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("x:");
+        int x = sc.nextInt();
+        System.out.print("y:");
+        int y = sc.nextInt();
+
+        System.out.println(x+"*"+y+"="+x*y);
+
+
+    }
+}
