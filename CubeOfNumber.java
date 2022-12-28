405b9b18ab3f61488daefc36bbd128e92aab6568
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2022-12-28 15:48:22 +0530

    Create CubeOfNumber.java

---
+++ CubeOfNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class CubeOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("give a number:");
+        int num = sc.nextInt();
+
+        System.out.println(num*num*num);
+
+
+    }
+}
