f630bc7ed7f53adff8582893deddc29ed94269e3
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2022-12-28 15:39:11 +0530

    Create RectangleArea.java

---
+++ RectangleArea.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("length of a rectangle:");
+        int length = sc.nextInt();
+
+        System.out.print("breadth of a rectangle:");
+        int breadth = sc.nextInt();
+
+        System.out.println("area of the rectangle is :"+ length*breadth);
+
+    }
+}
