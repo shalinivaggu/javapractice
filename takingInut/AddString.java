3cae5ce056c48c07448340ca28579406f97a1f57
vaggu shalini <94608109+shalinivaggu@users.noreply.github.com>
2022-12-29 17:39:20 +0530

    Create AddString.java

---
+++ takingInut/AddString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AddString {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("string 1 : ");
+        String str1 = sc.next();
+
+        System.out.println("string 2 : ");
+        String str2 = sc.next();
+
+        System.out.println(str1+str2);
+
+    }
+}
